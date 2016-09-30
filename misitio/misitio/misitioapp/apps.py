3a32b8598f0067bc9080ace0604f95053241c04e
Lexan <luis_grennby92@hotmail.com>
2016-09-30 13:15:12 -0600

    subida de proyecto django
    es solo un ejemplo

---
+++ misitio/misitio/misitioapp/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class MisitioappConfig(AppConfig):
+    name = 'misitioapp'
---
+++ misitio/misitio/misitioapp/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+import datetime
+from django.utils import timezone
+
+# Create your models here.
+
+class Pregunta(models.Model):
+    texto_pregunta = models.CharField(max_length=200)
+    fecha = models.DateTimeField('Fecha de publicación')
+
+class Opcion(models.Model):
+	pregunta = models.ForeignKey(Pregunta)
+	texto_opcion = models.CharField(max_length=200)
+	votos = models.IntegerField(default=0)
