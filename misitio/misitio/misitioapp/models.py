f498dea21b31af75e5fcc745d3e541a8d9eef8ec
Tuish <ortg_10@hotmail.com>
2016-09-30 18:29:14 -0600

    cambio en models.py

--- misitio/misitio/misitioapp/models.py
+++ misitio/misitio/misitioapp/models.py
@@ -6,9 +6,9 @@ from django.utils import timezone
 
 class Pregunta(models.Model):
     texto_pregunta = models.CharField(max_length=200)
-    fecha = models.DateTimeField('Fecha de publicación')
+    fecha = models.DateTimeField('Fecha que la publicamos')
 
-class Opcion(models.Model):
+class Correcta(models.Model):
 	pregunta = models.ForeignKey(Pregunta)
 	texto_opcion = models.CharField(max_length=200)
 	votos = models.IntegerField(default=0)
