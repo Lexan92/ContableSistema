00ba86167eea077a11b007e91308381491124c93
Tuish <ortg_10@hotmail.com>
2016-09-30 18:32:40 -0600

    modificacion en models.py

--- misitio/misitio/misitioapp/models.py
+++ misitio/misitio/misitioapp/models.py
@@ -8,7 +8,7 @@ class Pregunta(models.Model):
     texto_pregunta = models.CharField(max_length=200)
     fecha = models.DateTimeField('Fecha que la publicamos')
 
-class Correcta(models.Model):
+class Correct(models.Model):
 	pregunta = models.ForeignKey(Pregunta)
 	texto_opcion = models.CharField(max_length=200)
 	votos = models.IntegerField(default=0)
