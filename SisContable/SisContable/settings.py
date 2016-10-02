dd9a37b8cc3f5f9d17741c589120b29d50361303
Lexan <luis_grennby92@hotmail.com>
2016-10-02 11:10:39 -0600

    Configuracion de la base
    Porfavor revisar el nombre de la base "SIC" y los parametros siguientes
    segun su gestor.

--- SisContable/SisContable/settings.py
+++ SisContable/SisContable/settings.py
@@ -78,7 +78,7 @@ WSGI_APPLICATION = 'SisContable.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2', 
-       'NAME': 'misitio', 
+       'NAME': 'SIC', 
        'USER': 'postgres', 
         'PASSWORD': 'admin', 
         'HOST': 'localhost', 
